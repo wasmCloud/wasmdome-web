f7f34b3cf3885fc4292ceb172b59b960e83a02df
brooksmtownsend <btownsend@Brookss-MBP.fios-router.home>
2020-03-16 16:10:35 -0400

    Implement leaderboard UI, add points field, more sample data

--- lib/wasmdome/leaderboards.ex
+++ lib/wasmdome/leaderboards.ex
@@ -7,10 +7,14 @@ defmodule Wasmdome.Leaderboards do
 
     def list_entries do
         [
-            %Entry{name: "Kevin", avatar: "bot-5", wins: 30, losses: 21, draws: 5, 
-                    last_activity: "Won a match, 3 days ago"},
-            %Entry{name: "Brooks", avatar: "bot-3", wins: 0, losses: 112, draws: 5,
-                    last_activity: "Lost, 3 days ago"}
+            %Entry{name: "Kevin", avatar: "bot-5", wins: 10, losses: 21, draws: 5, 
+                    last_activity: "Won a match, 3 days ago", points: 1},
+            %Entry{name: "Brooks", avatar: "bot-3", wins: 21, losses: 10, draws: 5,
+                    last_activity: "Lost, 3 days ago", points: 2},
+            %Entry{name: "cool-guy", avatar: "bot-1", wins: 1000, losses: 0, draws: 0,
+                    last_activity: "Won, 1 days ago", points: 50},
+            %Entry{name: "dweeb", avatar: "bot-4", wins: 1, losses: 1, draws: 20,
+                    last_activity: "Draw, 1 days ago", points: 3}
         ]
     end
 end 
